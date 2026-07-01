605453e7ac5635fedf61e128241e83ea59f99fb6
Emerson VUMC <vumc@whatever.local>
2026-07-01 09:42:21 -0500

    pheML_simple now trains a model given to it on th command line with --model_type and generates a confusion matrix from it, then saves the matrix and names it after the --name argument

--- src/pheML_simple.py
+++ src/pheML_simple.py
@@ -6,8 +6,11 @@ Mostly just for testing custom models in the pheML code
 """
 
 from pheML_develop import train_model
+from plotting import plot_CM
+
 from pathlib import Path
 import sys,argparse
+
 import pandas as pd
 
 def process_args() -> argparse.Namespace:
@@ -15,7 +18,7 @@ def process_args() -> argparse.Namespace:
     
     parser.add_argument('--train_folder',default='results/enrichment_test_0310') #where X_train & y_train are stored. normally the standard test output
     parser.add_argument('--model_type',default='LR') #type of model (RF, CART, etc.). defaults to linear regression
-    #parser.add_argument('--','')
+    parser.add_argument('--name','0311') #prefix to use when saving files to avoid name conflicts
     
     args = parser.parse_args()
     
@@ -26,13 +29,18 @@ def main():
     args = process_args()
     train_path = Path(args.train_folder)
     model_type = args.model_type
-
-    X = pd.read_csv(train_path / "X_train.csv")
-    y = pd.read_csv(train_path / "y_train.csv")
-    print(X)
-    print(y)
-
-    #final_model = train_model()
+    prefix = args.name
+    #import training data
+    X_train = pd.read_csv(train_path / "X_train.csv")
+    y_train = pd.read_csv(train_path / "y_train.csv")
+    #import testing data
+    X_test = pd.read_csv(train_path / "X_test.csv")
+    y_test = pd.read_csv(train_path / "y_test.csv")
+    #train the model
+    model = train_model(X_train,y_train,model_type)
+    #plot a confusion matrix for the model using a function from plotting.py (which also saves it)
+    precision = plot_CM(model,X_test,y_test,train_path,'Trait',prefix)
+    
     
 if __name__ == '__main__':
     main()
